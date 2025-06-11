9677b9a4bf4324d7873851a0c10546a49f614183
Kikimor-rec <115701754+Kikimor-rec@users.noreply.github.com>
2025-06-11 13:43:59 +0200

    Improve site design and add Lost Mark block

--- src/lib/components/Block.svelte
+++ src/lib/components/Block.svelte
@@ -2,9 +2,9 @@
 	let { header, main, image, reverse = false } = $props();
 </script>
 
-<div class="grid w-full grid-cols-1 gap-4 sm:grid-cols-2">
-	<div class={['prose', reverse ? 'sm:order-last' : 'sm:order-first']}>
-		<h2 class="text-4xl font-bold uppercase">{header}</h2>
+<div class="grid w-full grid-cols-1 items-center gap-12 sm:grid-cols-2">
+       <div class={['prose lg:prose-xl text-gray-300', reverse ? 'sm:order-last' : 'sm:order-first']}>
+               <h2 class="mb-4 text-4xl font-bold uppercase text-indigo-300">{header}</h2>
 
 		{@render main()}
 	</div>
